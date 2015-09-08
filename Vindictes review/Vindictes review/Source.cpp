8f4173868c17dd06d94ba50f6cbe383ac571e6eb
optildeath <w.montero34@gmail.com>
2015-09-08 10:37:35 -0500

    Vindictus review
    assignment turned in at 9/8/2015

---
+++ Vindictes review/Vindictes review/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+/*Written Assignment #3: Write a detailed analysis on the last game that you played.
+Is it good? Is it bad? Why? The focus is on what does it mean for a game to be good or bad.*/
+int main() {
+	cout << "The last game I had played was nothing more then 'Vindictus'" <<
+		"I had the courtasy of playing this game for over a few week, yet I feel like I gained a full expireced gameplay." <<
+		"Befor I give you my thoughts on the game, I most firest come clean. I origanily got the game for its 'great fan servise it had'." <<
+		"Vindictus is one of those RPGMMO's I have no fear in recaminding to any friend though." <<
+		"Its a great hack and slase with that of a fantasy mid-evil theme(There being dragons and beats thype thing)." <<
+		"The game got my attention with its helpful tutorial. Made the transition all the more smoother." <<
+		"But what I did find very intresting was how they had there classes set up. Its not something i'm rather used to seeing MMO's." <<
+		"They had pre-sets,this was very claver and well exacuded. Most RPGMMo's I have expirenced playing let you pick from a sort of classes." <<
+		"But there plenty of skilled MMo players that know how to uptamised a class to the point of being OP(OverPowered)." <<
+		"I find that 'Vindictus' does this well with doing creative pre-sets, so that players espically new players don't have to go through a large learning cuerve. " <<
+		"Thoguh they have pre-setted classes, it does not mean you cant creativly coustamised them to the users accord." <<
+		"Most games that do this never come out well. Tt being that it gets rather disapointing to play with a charecter with no emotional attachment." <<
+		"If there there is no attachment, there is no real game if you had to ask me. Vindictus not just bistose this on it charecters though." <<
+		"They also so place it on there MPC's witch i find great. I have fallen in love with most of te game stoy plots and with most of the MPC's" <<
+		"They show more emotion then most of the people in the real world. The best part, there are no over whelming missions." <<
+		"In othere MMo's there are MPC's that will just bumbar you with missions without finishing your priviouse quest." <<
+		"Its anoying and tidiouse, it couses so much confusion that one can easyly lose track of the story, plot or the quest it self." <<
+		"Vindictus, does not do that everything is done by voidgs. Once your hero is done its back to the main world." <<
+		endl;
+	cout << "What makes this a great game!? The game gives the user a form of challange." <<
+		"All gamers love a challange. But not just challaging but also rewording. Any thing that makes you work for something should come with a reword." <<
+		"Its buitiful the grapics are amazing. Alot of people puts this to consideration" <<
+		"The gameplay is smooth and rather fast paste. Even though I only had the chance to play with one charceter class " <<
+		"I still found all the more fun. I have seen the other classes i would not mind getting my hands on them in the later future."<<
+		"What would make a bad game would basiclly be the oposite of evverything i just wrought.";
+	system("pause");
+}
